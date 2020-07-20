5ba342afd7777fcf6120d43ec0164b8b6f7941da
Tuan Vuong <vanhtuan0409@gmail.com>
2020-07-20 15:12:06 +0700

    add naive implementation

--- src/problems/longest_substring_without_repeating_characters.rs
+++ src/problems/longest_substring_without_repeating_characters.rs
@@ -1,11 +1,27 @@
 #[allow(dead_code)]
 pub struct Solution {}
 
+use std::cmp::max;
+use std::collections::HashSet;
+
 // Leetcode link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 impl Solution {
     #[allow(dead_code)]
     pub fn length_of_longest_substring(s: String) -> i32 {
-        0
+        let mut max_length: i32 = 0;
+        let chars: Vec<char> = s.chars().collect();
+        for i in 0..chars.len() {
+            let mut visited = HashSet::new();
+            for j in i..chars.len() {
+                if visited.contains(&chars[j]) {
+                    break;
+                } else {
+                    max_length = max(max_length, (j - i + 1) as i32);
+                    visited.insert(chars[j]);
+                }
+            }
+        }
+        max_length
     }
 }
 
@@ -16,6 +32,7 @@ mod tests {
     #[test]
     fn test() {
         assert_eq!(3, Solution::length_of_longest_substring("abcabcbb".into()));
-        assert_eq!(3, Solution::length_of_longest_substring("abcabcbb".into()));
+        assert_eq!(1, Solution::length_of_longest_substring("bbbbb".into()));
+        assert_eq!(3, Solution::length_of_longest_substring("pwwkew".into()));
     }
 }
